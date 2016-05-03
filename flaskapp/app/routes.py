00b88818c6693ffcb3f9cede3ea2386249e44b8b
Adam Towse <towsea@uni.coventry.ac.uk>
2016-05-03 20:40:03 +0000

    Added Flask and basic page that will run on port :8080

---
+++ flaskapp/app/routes.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template
+ 
+app = Flask(__name__)      
+ 
+@app.route('/')
+def home():
+  return render_template('home.html')
+ 
+if __name__ == '__main__':
+  port = int(os.getenv('PORT', 8080))
+  host = os.getenv('IP', '0.0.0.0')
+  app.run(port=port, host=host)
+  
