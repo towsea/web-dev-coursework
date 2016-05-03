708e3268e5cc1b1bf0ba47e6c86e5873998c1c5f
Adam Towse <towsea@uni.coventry.ac.uk>
2016-05-03 21:00:18 +0000

    Flask completed

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
  
+@app.route('/about')
+def about():
+  return render_template('about.html')
+ 
 if __name__ == '__main__':
   port = int(os.getenv('PORT', 8080))
   host = os.getenv('IP', '0.0.0.0')
   app.run(port=port, host=host)
-  
+ 
