9a9fb8ac67547350087ec14b9f827211b8d83e7d
Adam Towse <towsea@uni.coventry.ac.uk>
2016-05-04 15:09:38 +0000

    php added

---
+++ assets/PHP/contact-form.php
@@ -0,0 +1,36 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$from = 'Website Contact Form'; 
+		$to = 'towsea@uni.coventry.ac.uk.com'; 
+		$subject = 'Message from Contact';
+		
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Has the message been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+ 
+		// Sends the email if no errors occure
+		if (!$errName && !$errEmail && !$errMessage) {
+		if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+		} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+	}
+	}
+?>
