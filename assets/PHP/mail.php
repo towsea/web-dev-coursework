da97f347d2639c46e853b745946e6cb67a046205
Adam Towse <towsea@uni.coventry.ac.uk>
2016-05-06 09:12:45 +0000

    added comments and made some changes to files

--- assets/PHP/mail.php
+++ assets/PHP/mail.php
@@ -3,38 +3,36 @@
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
-		$from = 'Website Contact Form'; 
-		$to = 'towsea@uni.coventry.ac.uk.com'; 
-		$subject = 'Message from Contact';
+		$from = 'contact@website.com'; 
+		$to = 'addz.towse@gmail.com'; 
+		$subject = 'Message from contact form ';
 		
 		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
  
-		// Check if name has been entered
+		// Check if the name has been entered
 		if (!$_POST['name']) {
 			$errName = 'Please enter your name';
 		}
 		
-		// Check if email has been entered and is valid
+		// Check if email that has been entered and is valid
 		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 			$errEmail = 'Please enter a valid email address';
 		}
 		
-		//Has the message been entered
+		//Check if message is entered
 		if (!$_POST['message']) {
 			$errMessage = 'Please enter your message';
 		}
  
-		// Sends the email if no errors occure
-		if (!$errName && !$errEmail && !$errMessage) {
-		if (mail ($to, $subject, $body, $from)) {
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage) {
+	if (mail ($to, $subject, $body, $from)) {
 		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-		} else {
+	} else {
 		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-		}
-		}
-		}
-		
-		//Popup for message sent
-		echo '<script type="text/javascript">alert("Your message has been sent");</script>';
-		echo '<script type="text/javascript">location.replace("index.html");</script>';
+	}
+}
+	}
+    echo '<script type="text/javascript">alert("Thank you for your message, we will be in contact shortly.");</script>';
+    echo '<script type="text/javascript">location.replace("/index.html");</script>';
 ?>
--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -1,9 +1,9 @@
 import os
 from flask import Flask, render_template
  
-app = Flask(__name__)      
+app = Flask(__name__)
  
-@app.route('/')
+@app.route('/home')
 def home():
   return render_template('home.html')
  
